c09c89f71892dc992c08b268f8cf81d85e11c1e7
Warren D. Rezane <warzkie123@gmail.com>
2023-01-21 22:34:29 +0800

    Added the embed files.

--- containers/EmbedsContainer.ts
+++ containers/EmbedsContainer.ts
@@ -1,3 +1,12 @@
 import { Embed } from "../interfaces/Embed";
+import { ServerRulesEmbed } from "../embeds/#oasix/server-rules";
+import { WelcomeEmbed } from "../embeds/#oasix/welcome";
+import { LevelPerksEmbed } from "../embeds/#oasix/level-perks";
+import { ConfessionsInstructionsEmbed } from "../embeds/#how-to-confess/confessions-instructions";
 
-export const EmbedsContainer: Embed[] = [];
+export const EmbedsContainer: Embed[] = [
+  WelcomeEmbed,
+  ServerRulesEmbed,
+  LevelPerksEmbed,
+  ConfessionsInstructionsEmbed,
+];
