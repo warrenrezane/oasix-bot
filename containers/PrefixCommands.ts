9dcd0f44720bfb027b8359249d1f5bd1b7273a6c
Warren D. Rezane <warzkie123@gmail.com>
2023-01-21 22:34:18 +0800

    Updated with embed prefix command.

--- containers/PrefixCommands.ts
+++ containers/PrefixCommands.ts
@@ -1,8 +1,12 @@
 import { PrefixCommand } from "../interfaces/PrefixCommand";
 import { ConfessionsBan } from "../commands/prefix/confessions-ban";
 import { ConfessionsUnban } from "../commands/prefix/confessions-unban";
+import { Embed } from "../commands/prefix/embed";
+import { Embeds } from "../commands/prefix/embeds";
 
 export const PrefixCommands: PrefixCommand[] = [
   ConfessionsBan,
   ConfessionsUnban,
+  Embed,
+  Embeds,
 ];
