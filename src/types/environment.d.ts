6674f0c5ccacc42085b3ad6d1171f15fb36ef712
Warren D. Rezane <warzkie123@gmail.com>
2023-01-23 01:15:23 +0800

    Added confession_report_channel to env.

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -9,6 +9,7 @@ declare global {
       PREFIX: string;
       CONFESSION_CHANNEL_ID: string;
       CONFESSION_LOGS_CHANNEL_ID: string;
+      CONFESSION_REPORTS_CHANNEL_ID: string;
       DB_HOST: string;
       DB_USER: string;
       DB_PASS: string;
