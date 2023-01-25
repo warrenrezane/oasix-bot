f31a1b183c5ea39d9b473165a9023352623cc7c4
Warren D. Rezane <warzkie123@gmail.com>
2023-01-25 16:43:49 +0800

    Make 'mysql' param as optional.

--- src/interfaces/PrefixCommand.ts
+++ src/interfaces/PrefixCommand.ts
@@ -1,9 +1,9 @@
 import { Client, Message, MessageApplicationCommandData } from "discord.js";
-import { QuickDB } from "quick.db";
+import { MySQLDriver } from "quick.db";
 
 export interface PrefixCommand extends MessageApplicationCommandData {
   usage?: string;
   additionalInfo?: string;
   commandDescription: string;
-  run: (client: Client, message: Message, db: QuickDB) => void;
+  run: (client: Client, message: Message, mysql?: MySQLDriver) => void;
 }
--- src/interfaces/SlashCommand.ts
+++ src/interfaces/SlashCommand.ts
@@ -3,9 +3,13 @@ import {
   ChatInputApplicationCommandData,
   Client,
 } from "discord.js";
-import { QuickDB } from "quick.db";
+import { MySQLDriver } from "quick.db";
 
 export interface SlashCommand extends ChatInputApplicationCommandData {
   usage?: string;
-  run: (client: Client, interaction: CommandInteraction, db: QuickDB) => void;
+  run: (
+    client: Client,
+    interaction: CommandInteraction,
+    mysql?: MySQLDriver
+  ) => void;
 }
