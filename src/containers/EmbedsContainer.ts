0e936016d65827330011674c0457f63cfdd30859
Warren D. Rezane <warzkie123@gmail.com>
2023-01-26 14:30:16 +0800

    Added the role embeds.

--- src/containers/EmbedsContainer.ts
+++ src/containers/EmbedsContainer.ts
@@ -3,10 +3,28 @@ import { ServerRulesEmbed } from "../embeds/#oasix/server-rules";
 import { WelcomeEmbed } from "../embeds/#oasix/welcome";
 import { LevelPerksEmbed } from "../embeds/#oasix/level-perks";
 import { ConfessionsInstructionsEmbed } from "../embeds/#how-to-confess/confessions-instructions";
+import { AgeRoleEmbed } from "../embeds/#roles/age";
+import { GenderRoleEmbed } from "../embeds/#roles/gender";
+import { PingsRoleEmbed } from "../embeds/#roles/pings";
+import { StatusRoleEmbed } from "../embeds/#roles/status";
+import { LocationRoleEmbed } from "../embeds/#roles/location";
+import { GamesRoleEmbed } from "../embeds/#roles/games";
+import { EconomyRoleEmbed } from "../embeds/#roles/economy";
+import { DMSRoleEmbed } from "../embeds/#roles/dms";
 
 export const EmbedsContainer: Embed[] = [
   WelcomeEmbed,
   ServerRulesEmbed,
   LevelPerksEmbed,
   ConfessionsInstructionsEmbed,
+
+  // Roles
+  PingsRoleEmbed,
+  AgeRoleEmbed,
+  GenderRoleEmbed,
+  StatusRoleEmbed,
+  LocationRoleEmbed,
+  GamesRoleEmbed,
+  EconomyRoleEmbed,
+  DMSRoleEmbed,
 ];
