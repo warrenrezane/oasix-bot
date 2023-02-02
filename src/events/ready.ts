101a5d8c62adc4b7cacc4e3c0d16dcbd1086affa
Warren D. Rezane <warzkie123@gmail.com>
2023-02-03 01:30:57 +0800

    Added a quote-of-the-day cron job.

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,7 @@
 import { Client, ComponentType, TextChannel } from "discord.js";
 import { Commands } from "../containers/Commands";
+import https from "https";
+import * as schedule from "node-schedule";
 
 const date = new Date();
 
@@ -18,5 +20,36 @@ export default (client: Client): void => {
     await client.application.commands.set(Commands);
 
     console.log(`[${date.toUTCString()}] ${client.user.tag} is online!`);
+
+    const rule = new schedule.RecurrenceRule();
+    rule.hour = 9;
+    rule.tz = "Asia/Manila";
+
+    schedule.scheduleJob(rule, function () {
+      https
+        .get("https://api.quotable.io/random?limit=1", (response) => {
+          let data = "";
+
+          response.on("data", (chunk) => {
+            data += chunk;
+          });
+
+          response.on("end", () => {
+            const { content, author } = JSON.parse(data);
+            (
+              client.channels.cache.get(
+                process.env.BOT_TESTING_CHANNEL_ID as string
+              ) as TextChannel
+            ).send({
+              content: `**QOUTE OF THE DAY:**\n\n*"${content}"*\n- ${author}\n\nGood Morning, <@&${
+                process.env.OASIX_VERIFIED_ROLE as string
+              }>.`,
+            });
+          });
+        })
+        .on("error", (error) => {
+          console.error(error);
+        });
+    });
   });
 };
