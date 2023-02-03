52eb28931251b18e3bcfbf05064ec50eabd2eab2
Warren D. Rezane <warzkie123@gmail.com>
2023-02-03 08:50:24 +0000

    Moved the scheduler from ready.ts to index.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,7 +1,5 @@
-import { Client, ComponentType, TextChannel } from "discord.js";
+import { Client } from "discord.js";
 import { Commands } from "../containers/Commands";
-import https from "https";
-import * as schedule from "node-schedule";
 
 const date = new Date();
 
@@ -11,8 +9,7 @@ export default (client: Client): void => {
 
     Commands.forEach((command) => {
       console.log(
-        `[${date.toUTCString()}]\n[Command: ${
-          command.name
+        `[${date.toUTCString()}]\n[Command: ${command.name
         }] has been loaded, and is ready to use. \nUsage: ${command.usage}\n`
       );
     });
@@ -20,36 +17,5 @@ export default (client: Client): void => {
     await client.application.commands.set(Commands);
 
     console.log(`[${date.toUTCString()}] ${client.user.tag} is online!`);
-
-    const rule = new schedule.RecurrenceRule();
-    rule.hour = 9;
-    rule.tz = "Asia/Manila";
-
-    schedule.scheduleJob(rule, function () {
-      https
-        .get("https://api.quotable.io/random?limit=1", (response) => {
-          let data = "";
-
-          response.on("data", (chunk) => {
-            data += chunk;
-          });
-
-          response.on("end", () => {
-            const { content, author } = JSON.parse(data);
-            (
-              client.channels.cache.get(
-                process.env.BOT_TESTING_CHANNEL_ID as string
-              ) as TextChannel
-            ).send({
-              content: `**QOUTE OF THE DAY:**\n\n*"${content}"*\n- ${author}\n\nGood Morning, <@&${
-                process.env.OASIX_VERIFIED_ROLE as string
-              }>.`,
-            });
-          });
-        })
-        .on("error", (error) => {
-          console.error(error);
-        });
-    });
   });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
-import { Client, Partials, GatewayIntentBits, ActivityType } from "discord.js";
+import { Client, Partials, GatewayIntentBits, ActivityType, TextChannel } from "discord.js";
 import ready from "./events/ready";
 import interactionCreate from "./events/interactionCreate";
 import messageCreate from "./events/messageCreate";
 import { MySQLDriver } from "quick.db";
 import http from "http";
+import https from "https";
+import * as schedule from "node-schedule";
 
 export const client = new Client({
   intents: [
@@ -71,6 +73,37 @@ client.login(AuthenticationToken).catch((err) => {
     `[${date.toUTCString()}] [CRASH] Error from Discord API: ${err}`
   );
   return process.exit();
+});
+
+const rule = new schedule.RecurrenceRule();
+rule.hour = 9;
+rule.minute = 1;
+rule.tz = "Asia/Manila";
+
+schedule.scheduleJob(rule, function() {
+  https
+    .get("https://api.quotable.io/random?limit=1", (response) => {
+      let data = "";
+
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      response.on("end", () => {
+        const { content, author } = JSON.parse(data);
+        (
+          client.channels.cache.get(
+            process.env.MAIN_CHAT_CHANNEL_ID as string
+          ) as TextChannel
+        ).send({
+          content: `**QOUTE OF THE DAY:**\n\n*"${content}"*\n- ${author}\n\nGood Morning, <@&${process.env.OASIX_VERIFIED_ROLE as string
+            }>.`,
+        });
+      });
+    })
+    .on("error", (error) => {
+      console.error(error);
+    });
 });
 
 process.on("unhandledRejection", async (err, promise) => {
