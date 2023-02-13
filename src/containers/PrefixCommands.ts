339d65cd3b8f8589b009b05d8381eff70ab7a015
Warren D. Rezane <warzkie123@gmail.com>
2023-02-13 23:39:42 +0800

    Temporarily removed confession ban/unban.

--- src/containers/PrefixCommands.ts
+++ src/containers/PrefixCommands.ts
@@ -3,10 +3,14 @@ import { ConfessionsBan } from "../commands/prefix/confessions-ban";
 import { ConfessionsUnban } from "../commands/prefix/confessions-unban";
 import { Embed } from "../commands/prefix/embed";
 import { Embeds } from "../commands/prefix/embeds";
+import { Tag } from "../commands/prefix/tag";
+import { Tags } from "../commands/prefix/tags";
 
 export const PrefixCommands: PrefixCommand[] = [
-  ConfessionsBan,
-  ConfessionsUnban,
+  // ConfessionsBan,
+  // ConfessionsUnban,
   Embed,
   Embeds,
+  Tag,
+  Tags,
 ];
