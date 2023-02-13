757d68467de3334ea39022227f8d7a9a2484289f
Warren D. Rezane <warzkie123@gmail.com>
2023-02-13 23:40:47 +0800

    Removed tags from slash commands.

--- src/containers/SlashCommands.ts
+++ src/containers/SlashCommands.ts
@@ -1,6 +1,4 @@
 import { Confess } from "../commands/slash/confess";
-import { Tag } from "../commands/slash/tag";
-import { Tags } from "../commands/slash/tags";
 import { SlashCommand } from "../interfaces/SlashCommand";
 
-export const SlashCommands: SlashCommand[] = [Confess, Tag, Tags];
+export const SlashCommands: SlashCommand[] = [Confess];
